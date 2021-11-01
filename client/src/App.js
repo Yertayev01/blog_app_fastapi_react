537fc7a2d46149e3328147d5951ad10ec7a5c0f9
Corbin Robb <corbin@Corbins-MacBook-Pro.local>
2021-11-01 14:44:52 -0600

    Create React App for frontend and set up tailwindcss

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+function App() {
+  return (
+    <div className="font-sans bg-gray-50	h-screen">
+      <header className="border-r-4 border-l-4 border-gray-400 bg-gray-200 pb-20 pt-5 filter drop-shadow-md">
+        <title className="border border-indigo-200 m-5 py-5 filter drop-shadow rounded-md flex justify-center bg-indigo-100">
+          <p className="text-5xl uppercase text-white filter drop-shadow-sm">
+            Place Holder
+          </p>
+        </title>
+      </header>
+    </div>
+  );
+}
+
+export default App;
