d77322267a7f378b411a6ba085c86bea1caa10f6
Corbin Robb <corbin@Corbins-MacBook-Pro.local>
2021-11-04 23:34:06 -0600

    Add authentication and hashing

--- app/crud.py
+++ app/crud.py
@@ -1,5 +1,5 @@
 from sqlalchemy.orm import Session
-from . import models, schemas
+from . import models, schemas, auth
 
 
 def get_user(db: Session, user_id: int):
@@ -13,8 +13,8 @@ def get_users(db: Session, skip: int = 0, limit: int = 100):
 
 
 def create_user(db: Session, user: schemas.UserCreate):
-    fake_hashed = user.password + "fakehash"
-    db_user = models.User(email=user.email, hashed_password=fake_hashed)
+    hashed_password = auth.get_password_hash(user.password)
+    db_user = models.User(email=user.email, hashed_password=hashed_password)
     db.add(db_user)
     db.commit()
     db.refresh(db_user)
--- app/main.py
+++ app/main.py
@@ -1,21 +1,26 @@
-from typing import List
+from typing import List, Optional
 
-from fastapi import Depends, FastAPI, HTTPException
+from fastapi import Depends, FastAPI, HTTPException, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.security import OAuth2PasswordRequestForm
 from sqlalchemy.orm import Session
 
-from . import crud, models, schemas
+from . import crud, models, schemas, auth
 from .database import SessionLocal, engine
+from .dependencies import get_db
+
+from datetime import datetime, timedelta
+from .secret import SECRET_KEY, ALGORITHM, ACCESS_TOKEN_EXPIRE_MINUTES
 
 # very simplistic way to create the tables
 # should use Alembic to create tables and handle migrations
+# TODO Set up Alembic
 models.Base.metadata.create_all(bind=engine)
-
-app = FastAPI()
 
 # To run:
 # navigate to root folder
 # run `uvicorn app.main:app --reload`
+app = FastAPI()
 
 origins = ["http://localhost:3000"]
 
@@ -26,15 +31,6 @@ app.add_middleware(
     allow_methods=["*"],
     allow_headers=["*"]
 )
-
-# Dependency
-def get_db():
-    db = SessionLocal()
-    try:
-        yield db
-    finally:
-        db.close()
-
 
 
 @app.post("/users/", response_model=schemas.User)
@@ -61,6 +57,27 @@ def create_item(user_id: int, item: schemas.ItemCreate, db: Session = Depends(ge
     return crud.create_item(db, item=item, user_id=user_id)
 
 @app.get("/items/", response_model=List[schemas.Item])
-def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
+def read_items(skip: int = 0, limit: int = 100, db: Session = Depends(get_db), token: str = Depends(auth.oauth2_scheme)):
     items = crud.get_items(db, skip=skip, limit=limit)
     return items
+
+
+@app.post("/token", response_model=schemas.Token)
+async def login_for_access_token(form_data: OAuth2PasswordRequestForm = Depends(), db: Session = Depends(get_db)):
+    user = auth.authenticate_user(db, form_data.username, form_data.password)
+    if not user:
+        raise HTTPException(
+            status_code=status.HTTP_401_UNAUTHORIZED,
+            detail="Incorrect username or password",
+            headers={"WWW-Authenticate": "Bearer"},
+        )
+    access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
+    access_token = auth.create_access_token(
+        data={"sub": user.email}, expires_delta=access_token_expires
+    )
+    return {"access_token": access_token, "token_type": "bearer"}
+
+
+@app.get("/token/user")
+async def read_users_me(current_user: schemas.User = Depends(auth.get_current_active_user)):
+    return current_user
