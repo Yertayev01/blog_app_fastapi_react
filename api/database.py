cbd51f985faeef5cb4b8beb74eb19afc5ac08f16
Corbin Robb <corbin@Corbins-MacBook-Pro.local>
2021-11-07 20:53:19 -0700

    Remove old main.py and clean up some comments

--- api/database.py
+++ api/database.py
@@ -5,14 +5,8 @@ import os
 
 load_dotenv()
 
-# SQLALCHEMY_DATABASE_URL = "sqlite:///./simple_api.db"
 SQLALCHEMY_DATABASE_URL = os.getenv("DATABASE_URL")
 # connect_args={"check_same_thread": False} is only needed for sqlite
 
-# print(os.getenv("DATABASE_URL"))
-# postgresql+psycopg2://postgres:password@:5432/methinky_db
-
 engine = create_engine(SQLALCHEMY_DATABASE_URL)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
-
-# Base = declarative_base()
